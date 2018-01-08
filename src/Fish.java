c1f15327671e56d851cdc147cc096f22f1a53768
karta1902613 <105021043@live.asia.edu.tw>
2018-01-08 23:01:32 +0800

    0
    0

--- src/Fish.java
+++ src/Fish.java
@@ -3,40 +3,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
-class Fish extends JLabel implements Runnable{
+public class Fish extends JLabel implements Runnable{
     private int frmW , frmH,x,y,r1,r;
     private ImageIcon[][] imgIcons = {{new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png")},{new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png"),new ImageIcon("fish.png")}};
-    private boolean dirFlag = true;
+   // private boolean dirFlag = true;
     private Timer t1;
     private Random rand = new Random();
     public Fish(int frmH, int frmW){
         this.frmH = frmH;
         this.frmW = frmW;
-        x = rand.nextInt(frmW-100);
-        y = rand.nextInt(frmH-100);
-        r = rand.nextInt(2);
-        if(r==1){
-            this.dirFlag = false;
-        }
-        this.setIcon(imgIcons[r][r1 = rand.nextInt(5)]);
+        x = rand.nextInt(1000-100);
+        y = rand.nextInt(900-100);
+    //    r = rand.nextInt(2);
+   //     if(r==1){
+     //       this.dirFlag = false;
+     //   }
+        this.setIcon(imgIcons[1][r1 = rand.nextInt(5)]);
         this.setBounds(x,y,this.getIcon().getIconWidth(),this.getIcon().getIconHeight());
+
     }
     @Override
     public void run(){
+
         t1 = new Timer(rand.nextInt(1000)+50, new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                if(Fish.this.dirFlag){
-                    if((x-10)>0) {
-                        x -= 10;
-                    }else{
-                        Fish.this.dirFlag = !Fish.this.dirFlag;
-                        r=1;
-                        Fish.this.setIcon(imgIcons[r][r1]);
-                        x-=10;
-                    }
+
+                    System.out.println(x);
+
+                        x += 30;
+                        if(x>1000){
+                            t1.stop();
+                        }
                     Fish.this.setLocation(x,y);
-                }
+
 
             }
         });
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,10 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.ArrayList;
-import java.util.Random;
+
 public class MainFrame extends JFrame{
     private Container cp;
     private JPanel jpl = new JPanel();
@@ -13,8 +11,8 @@ public class MainFrame extends JFrame{
     private JButton jbtnAddFish = new JButton("Add Fish");
     private JButton jbtnExit = new JButton("Exit");
     private JLabel jlb = new JLabel("");
-    private int imgW,imgH;
-    private int fishIndex = 0;
+  //  private int imgW,imgH;
+  //  private int fishIndex = 0;
     private ArrayList <Fish> fishList = new ArrayList<Fish>();
    //    private ArrayList <Fish2> fishList2 = new ArrayList<Fish2>();
     private ArrayList <Thread> threadList = new ArrayList<Thread>();
@@ -30,7 +28,7 @@ public class MainFrame extends JFrame{
         cp.add(jpl, BorderLayout.CENTER);
         jpl.setLayout(null);
         this.setBounds(350, 100, 1000, 900);
-        this.setResizable(false);
+       // this.setResizable(false);
         toolPane.add(jbtnAddFish);
         toolPane.add(jbtnExit);
         jlb.setBounds(100, 100, 80, 30);
